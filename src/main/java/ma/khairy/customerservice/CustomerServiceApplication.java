be511f4fe1810547c3670bddaac07131c6b42a8a
Sami-Khairy <samikhairy2003@gmail.com>
2024-11-20 15:22:00 +0100

    app config (h2 db & port)

--- src/main/java/ma/khairy/customerservice/CustomerServiceApplication.java
+++ src/main/java/ma/khairy/customerservice/CustomerServiceApplication.java
@@ -1,7 +1,11 @@
 package ma.khairy.customerservice;
 
+import ma.khairy.customerservice.entities.Customer;
+import ma.khairy.customerservice.repository.CustomerRepo;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CustomerServiceApplication {
@@ -10,4 +14,12 @@ public class CustomerServiceApplication {
         SpringApplication.run(CustomerServiceApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner start(CustomerRepo customerRepo) {
+        return args -> {
+            customerRepo.save(Customer.builder().name("khaled").email("k@k.com").build());
+            customerRepo.save(Customer.builder().name("mohamed").email("m@m.com").build());
+            customerRepo.save(Customer.builder().name("ahmed").email("a@a.com").build());
+        };
+    }
 }
